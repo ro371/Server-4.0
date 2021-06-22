f18b99471cde6b8d372dad8ac9f9ce8ec193514b
ro371 <rogel@DESKTOP-TVBUE3M.mshome.net>
2021-06-22 15:03:26 +0200

    Añadidos comandos para atar y soltar

---
+++ src/main/java/net/mcreator/server/procedures/AtarCommandExecutedProcedure.java
@@ -0,0 +1,122 @@
+package net.mcreator.server.procedures;
+
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.World;
+import net.minecraft.world.IWorld;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.util.math.vector.Vector2f;
+import net.minecraft.entity.Entity;
+import net.minecraft.command.ICommandSource;
+import net.minecraft.command.CommandSource;
+
+import net.mcreator.server.ServerModElements;
+import net.mcreator.server.ServerMod;
+
+import java.util.Map;
+import java.util.HashMap;
+
+@ServerModElements.ModElement.Tag
+public class AtarCommandExecutedProcedure extends ServerModElements.ModElement {
+	public AtarCommandExecutedProcedure(ServerModElements instance) {
+		super(instance, 244);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				ServerMod.LOGGER.warn("Failed to load dependency entity for procedure AtarCommandExecuted!");
+			return;
+		}
+		if (dependencies.get("cmdparams") == null) {
+			if (!dependencies.containsKey("cmdparams"))
+				ServerMod.LOGGER.warn("Failed to load dependency cmdparams for procedure AtarCommandExecuted!");
+			return;
+		}
+		if (dependencies.get("x") == null) {
+			if (!dependencies.containsKey("x"))
+				ServerMod.LOGGER.warn("Failed to load dependency x for procedure AtarCommandExecuted!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			if (!dependencies.containsKey("y"))
+				ServerMod.LOGGER.warn("Failed to load dependency y for procedure AtarCommandExecuted!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			if (!dependencies.containsKey("z"))
+				ServerMod.LOGGER.warn("Failed to load dependency z for procedure AtarCommandExecuted!");
+			return;
+		}
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				ServerMod.LOGGER.warn("Failed to load dependency world for procedure AtarCommandExecuted!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		HashMap cmdparams = (HashMap) dependencies.get("cmdparams");
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		IWorld world = (IWorld) dependencies.get("world");
+		if ((((entity.getDisplayName().getString())).equals("Dev"))) {
+			if (world instanceof ServerWorld) {
+				((World) world).getServer().getCommandManager().handleCommand(
+						new CommandSource(ICommandSource.DUMMY, new Vector3d(x, y, z), Vector2f.ZERO, (ServerWorld) world, 4, "",
+								new StringTextComponent(""), ((World) world).getServer(), null).withFeedbackDisabled(),
+						(("/replaceitem entity ") + "" + ((((new Object() {
+							public String getText() {
+								String param = (String) cmdparams.get("0");
+								if (param != null) {
+									return param;
+								}
+								return "";
+							}
+						}.getText())) + "" + (" armor.head server:preso_armor_helmet{Enchantments:[{id:\"minecraft:binding_curse\",lvl:1}]}")))));
+			}
+			if (world instanceof ServerWorld) {
+				((World) world).getServer().getCommandManager().handleCommand(
+						new CommandSource(ICommandSource.DUMMY, new Vector3d(x, y, z), Vector2f.ZERO, (ServerWorld) world, 4, "",
+								new StringTextComponent(""), ((World) world).getServer(), null).withFeedbackDisabled(),
+						(("/replaceitem entity ") + "" + ((((new Object() {
+							public String getText() {
+								String param = (String) cmdparams.get("0");
+								if (param != null) {
+									return param;
+								}
+								return "";
+							}
+						}.getText())) + ""
+								+ (" armor.chest server:preso_armor_chestplate{Enchantments:[{id:\"minecraft:binding_curse\",lvl:1}]}")))));
+			}
+			if (world instanceof ServerWorld) {
+				((World) world).getServer().getCommandManager().handleCommand(
+						new CommandSource(ICommandSource.DUMMY, new Vector3d(x, y, z), Vector2f.ZERO, (ServerWorld) world, 4, "",
+								new StringTextComponent(""), ((World) world).getServer(), null).withFeedbackDisabled(),
+						(("/replaceitem entity ") + "" + ((((new Object() {
+							public String getText() {
+								String param = (String) cmdparams.get("0");
+								if (param != null) {
+									return param;
+								}
+								return "";
+							}
+						}.getText())) + "" + (" armor.legs server:preso_armor_leggings{Enchantments:[{id:\"minecraft:binding_curse\",lvl:1}]}")))));
+			}
+			if (world instanceof ServerWorld) {
+				((World) world).getServer().getCommandManager().handleCommand(
+						new CommandSource(ICommandSource.DUMMY, new Vector3d(x, y, z), Vector2f.ZERO, (ServerWorld) world, 4, "",
+								new StringTextComponent(""), ((World) world).getServer(), null).withFeedbackDisabled(),
+						(("/replaceitem entity ") + "" + ((((new Object() {
+							public String getText() {
+								String param = (String) cmdparams.get("0");
+								if (param != null) {
+									return param;
+								}
+								return "";
+							}
+						}.getText())) + "" + (" armor.feet server:preso_armor_boots{Enchantments:[{id:\"minecraft:binding_curse\",lvl:1}]}")))));
+			}
+		}
+	}
+}
